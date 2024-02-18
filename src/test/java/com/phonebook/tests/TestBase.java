5d9aad267850d46c5f4f41d42c3f416d686d8972
Sandor Ivanyo <139584544+SancosJak@users.noreply.github.com>
2024-02-18 16:54:05 +0100

    update

--- src/test/java/com/phonebook/tests/TestBase.java
+++ src/test/java/com/phonebook/tests/TestBase.java
@@ -2,20 +2,34 @@ package com.phonebook.tests;
 
 import com.phonebook.fw.ApplicationManager;
 import org.openqa.selenium.remote.Browser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+
+import java.lang.reflect.Method;
 
 public class TestBase {
 
     protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", Browser.CHROME.browserName()));
 
-    //    @BeforeMethod
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
+    @BeforeMethod
+    public void startTest(Method method){
+        logger.info("Start test " + method.getName());
+    }
+    @AfterMethod
+    public  void stopTest(){
+        logger.info("Stop test");
+        logger.info("**********************************************");
+    }
     @BeforeSuite
     public void setUp() {
         app.init();
     }
 
-    //    @AfterMethod(enabled = true)
     @AfterSuite
     public void tearDown() {
         app.stop();
